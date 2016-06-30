4803fedac91bb95fb3b24e7a1f79fe3bdf9813f6
Michael Sarchet <mikesarchet@gmail.com>
2016-06-30 10:16:16 -0700

    Add some comments to the tests

--- __tests__/components/app.test.js
+++ __tests__/components/app.test.js
@@ -1,13 +1,17 @@
+// We must mock the styles so that CSSModules plays nice
 jest.mock('../../src/components/styles/app.css')
+// We must unmock the thing we actually want to test
 jest.unmock('../../src/components/app')
 
 import React from 'react'
+// this has all the test goodies
 import { shallow } from 'enzyme'
 
+// bring in any components we wist to use
 import App from '../../src/components/app'
 
 describe('App', () => {
-  // this is not ideal
+  // Check that app renders as we think it should
   it('is an h1 and has its children', () => {
     const app = shallow(<App />)
 
